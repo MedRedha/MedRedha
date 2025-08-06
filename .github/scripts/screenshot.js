083353bef509c44c6353e0a54b4d74124dabece1
Med Redha <redha.dirac@gmail.com>
2025-08-06 07:45:34 +0200

    🔧 enhanced prayer times bot

--- .github/scripts/screenshot.js
+++ .github/scripts/screenshot.js
@@ -8,7 +8,7 @@ const fs = require("fs");
   const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
   const page = await browser.newPage();
 
-  await page.setViewport({ width: 800, height: 600 });
+  await page.setViewport({ width: 600, height: 650 });
 
   const widgetUrl = 'https://timesprayer.com/widgets.php?frame=1&lang=en&name=berlin&time=0&fcolor=32146A&tcolor=414692&frcolor=4ABC4A';
 
